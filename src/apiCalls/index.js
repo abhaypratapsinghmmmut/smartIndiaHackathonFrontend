e5fc1962a01f50b857bcca03b353b7b1d0dad37e
abhaypratapsinghmmmut <abhaypratapsinghmmmut@gmail.com>
2025-09-30 00:18:17 +0530

    final changes

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "https://smartindiahackathon.onrender.com",
+    baseURL: "https://smartindiahackathonbackend.onrender.com",
 });
